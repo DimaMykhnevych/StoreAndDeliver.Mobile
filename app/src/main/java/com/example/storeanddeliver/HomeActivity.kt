d537366deb960654e5f14d14f4e2510c6abaf362
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2021-11-25 09:32:49 +0200

    Add navigation for user

--- app/src/main/java/com/example/storeanddeliver/HomeActivity.kt
+++ app/src/main/java/com/example/storeanddeliver/HomeActivity.kt
@@ -2,10 +2,41 @@ package com.example.storeanddeliver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.storeanddeliver.fragments.IndicatorsFragment
+import com.example.storeanddeliver.fragments.Requests
+import com.google.android.material.bottomnavigation.BottomNavigationMenu
+import android.widget.AdapterView.OnItemSelectedListener
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    private val requestsFragment = Requests()
+    private val indicatorsFragment = IndicatorsFragment()
+    private var bottomNavigation: BottomNavigationView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        replaceFragment(requestsFragment)
+        bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigation?.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_indicators -> replaceFragment(indicatorsFragment)
+                R.id.ic_cargo -> replaceFragment(requestsFragment)
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        if(fragment != null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
     }
 }
