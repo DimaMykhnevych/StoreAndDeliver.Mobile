c6a4398e4245cf9ef03b0637eaadaf8b7fbe6424
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2021-11-30 10:08:16 +0200

    Add header, progress bar and empty indicators view elements

--- app/src/main/java/com/example/storeanddeliver/fragments/IndicatorsFragment.kt
+++ app/src/main/java/com/example/storeanddeliver/fragments/IndicatorsFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import com.example.storeanddeliver.R
+import com.example.storeanddeliver.databinding.FragmentIndicatorsBinding
+import com.example.storeanddeliver.databinding.FragmentRequestsBinding
 import com.example.storeanddeliver.managers.UserSettingsManager
 import com.example.storeanddeliver.models.CargoRequest
 import com.example.storeanddeliver.models.GetCargoSnapshotModel
@@ -20,6 +23,8 @@ import okhttp3.Response
 class IndicatorsFragment : Fragment() {
     private var userCargoSnapshots:MutableList<UserCargoSnapshots> = mutableListOf()
     private var cargoSnapshotsService = CargoSnapshotsService()
+    private var _binding: FragmentIndicatorsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,13 +40,33 @@ class IndicatorsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_indicators, container, false)
+        _binding = FragmentIndicatorsBinding.inflate(inflater, container, false)
+        binding.progressBarIndicators.isVisible = true
+        binding.emptyIndicatorsText.visibility = View.GONE
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     private val onResponse: (Call, Response) -> Unit = { _, response ->
         val responseString = response.body!!.string()
         parseResponse(responseString)
-//        updateView()
+        updateView()
+    }
+
+    private fun updateView() {
+        activity?.runOnUiThread {
+            binding.progressBarIndicators.isVisible = false
+//            setupRequestsRecyclerView()
+            if (userCargoSnapshots.size == 0) {
+                binding.emptyIndicatorsText.visibility = View.VISIBLE
+            } else {
+                binding.emptyIndicatorsText.visibility = View.GONE
+            }
+        }
     }
 
     private fun parseResponse(response: String) {
