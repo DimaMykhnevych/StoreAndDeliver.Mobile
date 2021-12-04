dc872d2f9d46753aff77c6de6e7e1ce43695fc32
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2021-12-04 15:59:41 +0200

    Add company admin menu and carriers management fragment

--- app/src/main/java/com/example/storeanddeliver/HomeActivity.kt
+++ app/src/main/java/com/example/storeanddeliver/HomeActivity.kt
@@ -4,13 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.UserManager
+import android.view.View
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import com.example.storeanddeliver.constants.Roles
-import com.example.storeanddeliver.fragments.CarrierRequestsFragment
-import com.example.storeanddeliver.fragments.IndicatorsFragment
-import com.example.storeanddeliver.fragments.Requests
-import com.example.storeanddeliver.fragments.SettingsFragment
+import com.example.storeanddeliver.fragments.*
 import com.example.storeanddeliver.managers.CredentialsManager
 import com.example.storeanddeliver.managers.UserSettingsManager
 import com.example.storeanddeliver.models.CargoRequest
@@ -25,8 +23,10 @@ class HomeActivity : AppCompatActivity() {
     private val requestsFragment = Requests()
     private val indicatorsFragment = IndicatorsFragment()
     private val settingsFragment = SettingsFragment(null)
+    private val carriersManagementFragment = CarriersManagementFragment()
     private lateinit var carrierRequestsFragment: CarrierRequestsFragment
     private var bottomNavigation: BottomNavigationView? = null
+    private var companyAdminBottomNavigation: BottomNavigationView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,24 +34,53 @@ class HomeActivity : AppCompatActivity() {
         setLocale(resources, UserSettingsManager.currentLocale)
         setContentView(R.layout.activity_home)
         bottomNavigation = findViewById(R.id.bottom_navigation)
-        if (CredentialsManager.role == Roles.Carrier) {
-            bottomNavigation?.menu?.removeItem(R.id.ic_indicators)
-            bottomNavigation?.menu?.removeItem(R.id.ic_cargo)
-            replaceFragment(carrierRequestsFragment)
-        } else if (CredentialsManager.role == Roles.User) {
-            bottomNavigation?.menu?.removeItem(R.id.ic_carrier_requests)
-            replaceFragment(requestsFragment)
-        }
+        companyAdminBottomNavigation = findViewById(R.id.company_admin_bottom_navigation)
+        setupMenuItems()
+
         bottomNavigation?.setOnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.ic_indicators -> replaceFragment(indicatorsFragment)
                 R.id.ic_carrier_requests -> replaceFragment(carrierRequestsFragment)
                 R.id.ic_cargo -> replaceFragment(requestsFragment)
                 R.id.ic_settings -> replaceFragment(settingsFragment)
+                R.id.ic_carriers_management -> replaceFragment(carrierRequestsFragment)
+                R.id.ic_company_admin_settings -> replaceFragment(settingsFragment)
             }
             true
         }
 
+        companyAdminBottomNavigation?.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.ic_carriers_management -> replaceFragment(carriersManagementFragment)
+                R.id.ic_company_admin_settings -> replaceFragment(settingsFragment)
+            }
+            true
+        }
+
+    }
+
+    private fun setupMenuItems() {
+        if (CredentialsManager.role == Roles.CompanyAdmin) {
+            companyAdminBottomNavigation?.visibility = View.VISIBLE
+            bottomNavigation?.visibility = View.GONE
+            replaceFragment(carriersManagementFragment)
+        } else {
+            when (CredentialsManager.role) {
+                Roles.Carrier -> {
+                    bottomNavigation?.menu?.removeItem(R.id.ic_indicators)
+                    bottomNavigation?.menu?.removeItem(R.id.ic_cargo)
+                    companyAdminBottomNavigation?.visibility = View.GONE
+                    bottomNavigation?.visibility = View.VISIBLE
+                    replaceFragment(carrierRequestsFragment)
+                }
+                Roles.User -> {
+                    bottomNavigation?.menu?.removeItem(R.id.ic_carrier_requests)
+                    companyAdminBottomNavigation?.visibility = View.GONE
+                    bottomNavigation?.visibility = View.VISIBLE
+                    replaceFragment(requestsFragment)
+                }
+            }
+        }
     }
 
     private val onMapBtnClick: (CargoRequest) -> Unit = { cargoRequest ->
