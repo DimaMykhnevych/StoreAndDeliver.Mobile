991594b529d6e3e979dae8d891bb31b1a943dc53
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2021-12-05 14:07:42 +0200

    Add deployed backend routes

--- app/src/main/java/com/example/storeanddeliver/constants/Constants.kt
+++ app/src/main/java/com/example/storeanddeliver/constants/Constants.kt
@@ -1,8 +1,13 @@
 package com.example.storeanddeliver.constants
 
 object Constants {
-    const val baseApiUrl: String = "http://10.0.2.2/api"
-    const val scheme: String = "http"
-    const val authority: String = "10.0.2.2"
+//    const val baseApiUrl: String = "http://10.0.2.2/api"
+//    const val scheme: String = "http"
+//    const val authority: String = "10.0.2.2"
+//    const val base: String = "api"
+
+    const val baseApiUrl: String = "https://storeanddeliver.azurewebsites.net/api"
+    const val scheme: String = "https"
+    const val authority: String = "storeanddeliver.azurewebsites.net"
     const val base: String = "api"
 }
